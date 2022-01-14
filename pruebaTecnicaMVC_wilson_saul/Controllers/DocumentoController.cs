f41c633f294eb20ddba7ab1f00b59c9eff6a719e
Wilson Ludeña <cornejo758@gmail.com>
2022-01-14 14:06:58 -0500

    CRUD a base de datos relacional usando entity framework Agregar la ruta "/persona" o "/documento" para acceder al proyecto

---
+++ pruebaTecnicaMVC_wilson_saul/Controllers/DocumentoController.cs
@@ -0,0 +1,77 @@
+﻿using pruebaTecnicaMVC_wilson_saul.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pruebaTecnicaMVC_wilson_saul.Controllers
+{
+    public class DocumentoController : Controller
+    {
+        //objeto de tipo personaDocumentoEntities
+        CAPACITACIONEntities context = new CAPACITACIONEntities();
+
+        public ActionResult Index() {
+            return View(context.TipoDocumento.ToList());
+        }
+
+        public ActionResult CrearDocumento() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CrearDocumento(TipoDocumento model) {
+            if (!ModelState.IsValid)
+            {
+
+                return View();
+            }
+            {
+                context.TipoDocumento.Add(model);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+
+        public ActionResult EditarDocumento(int id)
+        {
+            var documento = context.TipoDocumento.Find(id);
+            return View(documento);
+        }
+
+        [HttpPost]
+        public ActionResult EditarDocumento(TipoDocumento model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            
+            {
+                context.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult DetalleDocumento(int id)
+        {
+            var detalleDoc = context.TipoDocumento.Find(id);
+            return View(detalleDoc);
+        }
+
+        //Al borrar un registro, se crea un conflicto
+        public ActionResult EliminarDocumento(int id) {
+            var documento = context.TipoDocumento.Find(id);
+
+            if(documento != null)
+            {
+                context.TipoDocumento.Remove(documento);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ pruebaTecnicaMVC_wilson_saul/Controllers/PersonaController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using pruebaTecnicaMVC_wilson_saul.Models;
+
+namespace pruebaTecnicaMVC_wilson_saul.Controllers
+{
+    public class PersonaController : Controller
+    {
+        //objeto de tipo personaDocumentoEntities
+        CAPACITACIONEntities context = new CAPACITACIONEntities();
+
+        // Metodo que lista a las personas
+        public ActionResult Index()
+        {
+            return View(context.Persona.ToList());
+        }
+
+        //Metodo para crear una persona
+        public ActionResult CrearPersona()
+        {
+            ViewBag.TipoDocumento = new SelectList(context.TipoDocumento.ToList(), "Id", "Abreviatura");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CrearPersona(Persona model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.TipoDocumento = new SelectList(context.TipoDocumento.ToList(), "Id", "Abreviatura");
+                return View();
+            }
+            else
+            {
+                var tipoDocumento = context.TipoDocumento.Find(model.TipoDocumentoId);
+                model.TipoDocumento = tipoDocumento;
+                context.Persona.Add(model);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+
+        public ActionResult EditarPersona(int id) {
+            ViewBag.TipoDocumento = new SelectList(context.TipoDocumento.ToList(), "Id", "Abreviatura");
+            var persona = context.Persona.Find(id); 
+            return View(persona); 
+        }
+
+        [HttpPost]
+        public ActionResult EditarPersona(Persona model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.TipoDocumento = new SelectList(context.TipoDocumento.ToList(), "Id", "Abreviatura");
+                return View();
+            }
+
+                var documento = context.TipoDocumento.Find(model.TipoDocumentoId);
+
+                model.TipoDocumento = documento;
+                context.Persona.Attach(model);
+                context.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+
+            
+        }
+        public ActionResult PersonaDetalle(int id)
+        {
+            var persona = context.Persona.Find(id);
+            return View(persona);
+        }
+
+        public ActionResult EliminarPersona(int id)
+        {
+            var persona = context.Persona.Find(id);
+            if(persona != null)
+            {
+                context.Persona.Remove(persona);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
